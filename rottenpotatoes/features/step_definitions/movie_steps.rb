b885a9fbe0ac054a090405bcc839446a23f1a8b1
nm3076 Makhija <nm3076@columbia.edu>
2021-10-18 21:20:48 +0000

    pg and r filter working

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,10 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-      Movie.create!(title: movie[:title], rating: movie[:rating],
-      release_date: movie[:release_date])
+      Movie.create(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -31,7 +30,14 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(", ").each do |r|
+      if uncheck
+          step %Q{I uncheck "ratings_#{r}"}
+      else
+          step %Q{I check "ratings_#{r}"}
+      end
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
